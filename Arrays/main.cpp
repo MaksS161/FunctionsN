256fb56e5f3fe42d834c9692154d831500ae621d
Admin <Admin@ADMIN>
2023-02-15 20:33:48 +0300

    encoding

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -2,16 +2,36 @@
 #include <iostream>
 using namespace std;
 
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
+void Sum(int arr[], const int n);
+
 void main()
 {
 	setlocale(LC_ALL, "");
 
 	const int n = 5;
 	int arr[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+
+
+}
+
+void FillRand(int arr[], int n)
+{
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 100;
+		arr[i] = rand();
 	}
+} 
+
+void Print(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
@@ -19,7 +39,25 @@ void main()
 	cout << endl;
 }
 
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sum(int arr[], const int n)
+{
 
+}
 
 
 
